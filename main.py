f459fd4e36bca3d40e0b63fa504d12ba6a24e779
Baiyazeed <m.baiyazeed.hassan@gmail.com>
2023-06-04 13:07:01 +0200

    Making more chnages to app configs

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SECCRET_KEY'] = os.environ.get('SECCRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
